0633cc65d1f79443689c4e9932da1e9d4657df59
Matt Packwood <mattpackwood@gmail.com>
2020-05-03 13:40:36 -0400

    Added comments

--- Dialer.sh
+++ Dialer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
+# While loop reads through a text file of numbers and execute the AppleScript script "sendMessage.applescript" with the text defined here
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p" 
 osascript /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-scripts/sendMessage.applescript $p "Who were you dropping presents off to?"
 done < /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-scripts/AandA.dat
-
